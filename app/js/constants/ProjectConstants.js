b33bd7e700c46bf16a048c99bd3e82d9196a0815
Josh Wymer <wymetyme@gmail.com>
2015-08-09 02:39:47 -0700

    delete some unused files

--- app/js/constants/ProjectConstants.js
+++
@@ -1,7 +0,0 @@
-var keyMirror = require('keymirror');
-
-module.exports = keyMirror({
-  PROJECT_SAVE: null,
-  PROJECT_QUICK_ADD_OPEN: null,
-  PROJECT_QUICK_ADD_CLOSE: null,
-});
--- app/js/constants/UserConstants.js
+++
@@ -1,7 +0,0 @@
-var keyMirror = require('keymirror');
-
-module.exports = keyMirror({
-  USER_SAVE: null,
-  USER_QUICK_ADD_OPEN: null,
-  USER_QUICK_ADD_CLOSE: null,
-});
